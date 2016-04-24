8b1207b2b1df17ff4bb3af724e2a151f47052f6d
dylan-nelson <dylan.nelson@rackspace.com>
2016-04-24 06:07:47 -0500

    Add pip install django

--- recipes/install_pip.rb
+++ recipes/install_pip.rb
@@ -1,6 +1,7 @@
 #
 # Cookbook Name:: django
-# Recipe:: default
+# Recipe:: install_pip
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-include_recipe 'django::install_package'
+package 'python3-pip'
+execute 'pip3 install django'
